3f9ba09eb472f7cd737073752baf4fb97203e805
Satya5122 <satya6309614151@gmail.com>
2020-12-12 11:59:47 +0530

    Firstupdate
    logs welcome message

---
+++ Number_wizard/Assets/Scripts/number_wizard_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class number_wizard_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Pick a Number");
+        Debug.Log("Highest number is : 1000");
+        Debug.Log("Lowest number is : 0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
