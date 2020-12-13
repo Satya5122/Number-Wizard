44fe4f8fd8261407291fa144dd6d07188726f144
Satya5122 <satya6309614151@gmail.com>
2020-12-13 11:10:31 +0530

    added caluculations and guessing the number

--- Number_wizard/Assets/Scripts/number_wizard_script.cs
+++ Number_wizard/Assets/Scripts/number_wizard_script.cs
@@ -1,21 +1,51 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class number_wizard_script : MonoBehaviour
 {
+    public int max;
+    public int min;
+    public int guess;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Welcome to Number Wizard");
+        
+
+        Debug.Log("Welcome to Number Wizard, Let me guess it.......");
         Debug.Log("Pick a Number");
-        Debug.Log("Highest number is : 1000");
-        Debug.Log("Lowest number is : 0");
+        Debug.Log("Highest number is : " + max);
+        Debug.Log("Lowest number is : " + min);
+        Debug.Log("Now i'll try to guess the number.If higher than the guess press Uparrow or if lower than the guess press Downarrow or if the guess is correct press space");
+        Debug.Log("is your number :" + guess);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            
+            min = guess;
+            guess = (min + max) / 2;
+            Debug.Log("is your number :" + guess);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+           
+            max = guess;
+            guess = (min + max) / 2;
+            Debug.Log("is your number :" + guess);
+
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+                {
+            Debug.Log("your number has been guessed");
+        }
         
     }
 }
